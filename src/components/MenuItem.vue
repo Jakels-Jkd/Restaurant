bc31360383ce4a55fa077b731e1d8138bd8a7a5c
Jamleck Kemoli <kemolijamleck@gmail.com>
2025-09-25 11:58:09 +0300

    third commit

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,6 +1,32 @@
 <script setup> 
-
+const menuItem = {
+   
+        name: 'Pizza',
+        image: "/images/menu/pizza-pepperoni-with-lentil-vegetable-soups.jpg",
+        price: 899,
+        description: "Delicious cheese pizza with fresh ingredients.",
+        category: "main course"
+    }
 </script> 
 <template>
-    <h1>Menu Item</h1>
+    <v-container v-container fluid class="d-flex fill-height" align="center">
+        <v-row>
+            <v-col md="6">
+                <v-card class="pa-6 rounded-lg" max-width="600" justify="center"> >
+                   <v-img :src="menuItem.image" cover height="500"></v-img>
+                </v-card>
+            </v-col>
+            <v-col md="6">
+                <v-card  class="pa-6 rounded-lg" max-width="600" justify="center"> </v-card>
+                <v-card-title>{{ menuItem.name }}</v-card-title>
+                <v-card-subtitle>{{ menuItem.price }}</v-card-subtitle>
+                <v-card-text>{{ menuItem.description }}</v-card-text>
+                <v-card-text> Quantity <v-text-field></v-text-field> </v-card-text>
+                <v-card-actions>
+                    <v-btn color="blue-darken-">Order</v-btn>
+                </v-card-actions>
+                    
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -1,57 +1,96 @@
-<script setup> 
+<script setup>
 
-</script> 
+//imports
+import { ref } from 'vue'
+
+ const showMenu = ref(false)
+
+ //models
+ const name = ref(nullull)
+ const phone = ref(null)
+    const noOfPeople = ref(null)
+    const reservationDate = ref(null)
+    const reservationTime = ref(null)
+    const location = ref(null)
+
+//functions
+//make reservaition - add reservation details to local storage
+function makeReservation(){
+    const reservation={
+        name:name.value,
+        phone:phone.value,
+        noOfPeople:noOfPeople.value,
+        date:reservationDate.value,
+        time:reservationTime.value,
+        location:location.value
+    }
+    try{
+        //save databon browser
+        localStorage.setItem(
+            'reservation',
+            JSON.stringify(reservation))
+
+        //to do: send to the backend 
+    }catch(err){
+        console.log('reservation failed',err)}
+}
+ </script>
+
+
 <template>
     <h1>Reservation</h1>
-<v-container>
+<v-container fluid class="d-flex fill-height" align="center">
         <v-row>
             <v-col>
-                <v-card max-width="600">
+                <v-card class="pa-6 rounded-lg" max-width="600" justify="center">
                     <v-row>
                         <p class="text-h5">Make a Reservation</p>
                     </v-row>
                     <v-row>
                         <v-col md="4">Name</v-col>
                         <v-col md="8">
-                            <v-text-field label="Name" ></v-text-field>
+                            <v-text-field label="name" type="name" v-model="name" ></v-text-field>
                         </v-col>
                     </v-row>
                    <v-row>
                         <v-col md="4">Phone</v-col>
                         <v-col md="8">
-                            <v-text-field label="2541234456" ></v-text-field>
+                            <v-text-field label="number" v-model="number" ></v-text-field>
                         </v-col>
                     </v-row>
                     <v-row>
-                        <v-col md="4">No. of People</v-col>
+                        <v-col md="4">NoOfPeople</v-col>
                         <v-col md="8">
-                            <v-text-field></v-text-field>
+                            <v-text-field label="NoOfPeople"  v-model="noOfPeople"></v-text-field>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col md="4">Date</v-col>
                         <v-col md="8">
-                            <v-text-field></v-text-field>
+                            <v-date-input></v-date-input>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col md="4">Time</v-col>
                         <v-col md="8">
-                            <v-text-field></v-text-field>
+                            <v-text-field :model-value="reservationTime"label="Picker in menu"
+                            prepend-icon="mdi-clock-time-four-outline"
+                            readonly  v-model="time">
+                        </v-text-field>
                         </v-col>
                         <v-row>
                         <v-col md="4">Location</v-col>
                         <v-col md="8">
-                            <v-radio-group>
+                            <v-radio-group v-model="location" in line>
                             <v-row>
                                 <v-col md="4">
                                     <v-radio label="Madaraka" value="Madaraka"></v-radio>
                                 </v-col>
                                 <v-col md="4">
-                                    <v-radio label="CBD" value="CBD"></v-radio>
+                                    <v-radio label="Karen" value="Karen"></v-radio>
                                 </v-col>
                                 <v-col md="4">
-                                    <v-radio label="Your Home" value="Your Home"></v-radio>
+                                    <v-radio label="Utawala" value="Utawala"></v-radio>
                                 </v-col>
                             </v-row>
                             </v-radio-group>
@@ -60,7 +99,7 @@
                     </v-row>
                     <v-row>
                         <v-col align="center">
-                            <v-btn>Make Reservation</v-btn>
+                            <v-btn @click="makeReservation()" >Make Reservation</v-btn>
                         </v-col>
                     </v-row>
                 </v-card>
